bd0442aeefe963342df49b87fe92dbcc508ef9b0
johpa881 <johpa881@su17-118.ad.liu.se>
2020-02-10 19:04:50 +0100

    asdasd

--- lab1/src/lab1/Item.java
+++ lab1/src/lab1/Item.java
@@ -1,40 +1,51 @@
 package lab1;
 
-public class Item {
-	private double weight;
-	private String name;
-	private int price;
+public abstract class Item {
+	private final double weight;
+	private final String name;
+	private final int price;
+	private int itemSlot;
+	private boolean equipped = false;
 	
-	public Item(double weight, String name, int price) {
-		super();
+	public Item(double weight, String name, int price, int itemSlot) {
 		this.weight = weight;
 		this.name = name;
 		this.price = price;
+		this.itemSlot = itemSlot;
 	}
 
 	public double getWeight() {
 		return weight;
 	}
 
-	public void setWeight(double weight) {
-		this.weight = weight;
-	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public int getPrice() {
 		return price;
 	}
 
-	public void setPrice(int price) {
-		this.price = price;
+
+	public boolean isEquipped() {
+		return equipped;
 	}
+
+	public void setEquipped(boolean equipped) {
+		this.equipped = equipped;
+	}
+
+	public int getItemSlot() {
+		return itemSlot;
+	}
+
+	public void setItemSlot(int itemSlot) {
+		this.itemSlot = itemSlot;
+	}
+	
+	
 	
 	public void printYourself() {
 		
--- lab1/src/lab1/Objective.java
+++ lab1/src/lab1/Objective.java
@@ -5,6 +5,7 @@ public class Objective extends Outdoor {
 	//konstruktor
 	public Objective(String name, String description_short, String description_long) {
 		super(name, description_short, description_long);
+		setWinGame(true);
 	}
 		
 	public void doCommand() {
